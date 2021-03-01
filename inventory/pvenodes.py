238a6ce5d7374ffdec7d08031d8bffd90b4d590f
Tom Ford <tomfordweb@gmail.com>
2021-03-01 07:49:04 -0500

    slight refactoring

--- inventory/pvenodes.py
+++ inventory/pvenodes.py
@@ -1,7 +1,10 @@
 #!/usr/bin/env python3
 
 '''
-Example custom dynamic inventory script for Ansible, in Python.
+A dynamic inventory script that will open an SSH session to the
+pve node and get a list of all running containers.
+
+It will the returned a detailed inventory setting up any group variables needed.
 '''
 
 import os
@@ -14,6 +17,13 @@ try:
 except ImportError:
     import simplejson as json
 
+def paramiko_connection(server, username, password):
+    ssh = paramiko.SSHClient()
+    ssh.load_system_host_keys()
+    ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+    ssh.connect(server, username=username, password=password)
+    return ssh;
+
 def exec_ssh_command(paramikoClient, command):
     stdin, stdout, stderr = paramikoClient.exec_command(command)
 
@@ -23,14 +33,64 @@ def exec_ssh_command(paramikoClient, command):
         "stderr": stderr
     }
 
-def vm_response_factory(line):
-    returnString = line.rstrip()
-    split = returnString.split()
-    return {
-        "id": split[0],
-        "hostname": split[1],
-        "ip": None
-    }
+
+class ProxmoxVmResponse():
+    def __init__(self, stdout_row):
+        returnString = stdout_row.rstrip()
+        split = returnString.split()
+
+        self.id = split[0]
+        self.hostname = split[1]
+        self.ip = None
+
+    def setIpAddress(self, paramiko_response):
+        self.ip = paramiko_response.get('stdout').read().decode('utf-8').rstrip()
+
+
+    def toJson(self):
+        return {
+            "id": self.id,
+            "hostname": self.hostname,
+            "ip": self.ip
+        }
+
+class ProxmoxNodeInventory():
+    def __init__(self, items):
+        self.container = items
+
+
+    def getContainersByHostnameStartsWith(self, search):
+        return [x for x in self.container if x.hostname.startswith(search)]
+
+
+    @staticmethod
+    def createFromContainerResponse(paramiko_response):
+        allContainersOutput = paramiko_response.get('stdout').readlines()
+        all_containers = list(map(ProxmoxVmResponse, allContainersOutput))
+        
+        return ProxmoxNodeInventory(all_containers)
+
+    def toJson(self):
+        return [node.toJson() for node in self.container]
+
+
+# TODO: Use objects, clean this logic up
+def generate_swarm_node_meta(swarm_manager_containers, swarm_node_containers):
+
+    hostvars = {}
+    # For every swarm node, determine which manager it belongs to and set a custom
+    # host variable for that node. This will let us create a join token and join the appropriate
+    # swarm in the playbook
+    for host in swarm_node_containers:
+        node_swarm_manager_id = host.hostname.split('-')[2]
+        swarm_manager = [x for x in swarm_manager_containers if x.hostname.endswith(node_swarm_manager_id)][0]
+        hostvars[host.ip] = {
+            "manager": swarm_manager.ip
+        }
+
+    return hostvars
+
+
 
 class ProxmoxVmInventory(object):
 
@@ -51,51 +111,36 @@ class ProxmoxVmInventory(object):
 
         print(json.dumps(self.inventory))
 
+
     # Example inventory for testing.
     def example_inventory(self):
-        server = 'homelab'
-        username='root'
-        password=os.getenv('PVE_ROOT_PASSWORD')
+        ssh = paramiko_connection('homelab', 'root', os.getenv('PVE_ROOT_PASSWORD'))
 
-        ssh = paramiko.SSHClient()
-        ssh.load_system_host_keys()
-        ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
-        ssh.connect(server, username=username, password=password)
-        # Get a list of active vms
+        # Retreive a list of all active containers running on the server.
         allContainersCommand  = exec_ssh_command(ssh, "pct list | grep 'running' | awk '{print $1, $3}'")
-        allContainersOutput = allContainersCommand['stdout'].readlines()
-        all_containers = list(map(vm_response_factory, allContainersOutput))
+
+        all_containers = ProxmoxNodeInventory.createFromContainerResponse(allContainersCommand)
 
         # This is a list of every VM with its IP address
-        for host in all_containers:
-            command = f"pct exec {host['id']} -- hostname -I | awk '{{print $1}}'"
+        for host in all_containers.container:
+            command = f"pct exec {host.id} -- hostname -I | awk '{{print $1}}'"
             hostIpCommand = exec_ssh_command(ssh, command)
-            host['ip'] = hostIpCommand['stdout'].read().decode('utf-8').rstrip()
+            host.setIpAddress(hostIpCommand)
 
-        # extract swarm managers
-        swarm_manager_containers = [x for x in all_containers if x['hostname'].startswith('swarm-manager')]
-        swarm_node_containers = [x for x in all_containers if x['hostname'].startswith('swarm-node')]
+        swarm_manager_containers = all_containers.getContainersByHostnameStartsWith('swarm-manager')
+        swarm_node_containers = all_containers.getContainersByHostnameStartsWith('swarm-node')
 
-        hostvars = {}
-        # For every swarm node, determine which manager it belongs to and set a custom
-        # host variable for that node. This will let us create a join token and join the appropriate
-        # swarm in the playbook
-        for host in swarm_node_containers:
-            node_swarm_manager_id = host['hostname'].split('-')[2]
-            swarm_manager = [x for x in swarm_manager_containers if x['hostname'].endswith(node_swarm_manager_id)][0]
-            hostvars[host['ip']] = {
-                    "manager": swarm_manager['ip']
-                }
-
+        
+        hostvars = generate_swarm_node_meta(swarm_manager_containers, swarm_node_containers)
 
         return {
             'swarmmanagers': {
-                'hosts': [host['ip'] for host in swarm_manager_containers],
+                'hosts': [host.ip for host in swarm_manager_containers],
                 'vars': {
                 }
             },
             'swarmnodes': {
-                'hosts': [host['ip'] for host in swarm_node_containers],
+                'hosts': [host.ip for host in swarm_node_containers],
                 'vars': {
                 }
             },
